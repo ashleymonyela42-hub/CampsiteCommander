c2e423a3208fca7615a4b92eabd368df486a42f5
ashleymonyela42-hub <ashleymonyela42@gmail.com>
2026-06-10 09:55:04 +0200

    initial commit

--- app/src/main/java/com/example/campsitecommander/MainScreen.kt
+++ app/src/main/java/com/example/campsitecommander/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,29 +20,12 @@ class MainScreen : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CampsiteCommanderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Add gear")
                 }
+                
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CampsiteCommanderTheme {
-        Greeting("Android")
-    }
-}
